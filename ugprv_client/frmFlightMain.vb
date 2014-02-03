22cd6a762fa4da3c83d453393a36e099e55df4d9
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-02-03 00:08:39 +0000

    Working on RTSP stream viewing

--- ugprv_client/frmFlightMain.vb
+++ ugprv_client/frmFlightMain.vb
@@ -1,6 +1,7 @@
 ﻿Imports ugprv_ui_elements
 Imports ugprv_hid_connector
 Imports ugprv_hid_connector.GamePad
+Imports Media
 
 Public Class frmFlightMain
     Protected HIDGamePad As New ugprv_hid_connector.GamePad()
@@ -19,6 +20,44 @@ Public Class frmFlightMain
         HIDPoll.Start()
 
         Debug.WriteLine("[HID] HID Connector Running!")
+
+
+
+        'Camera stream stuff
+        Debug.WriteLine("[Media] Configuring RTSP Streaming Client...")
+        Dim CamHost As String = "rtsp://127.0.0.1:8554/" '"rtsp://77.68.36.93:5661/Feed"
+
+        Dim CamAURI As String = CStr(CamHost & "")
+        Dim CamBURI As String = CStr(CamHost & "B")
+        Dim CamCURI As String = CStr(CamHost & "C")
+
+        Dim CamAStream As New Media.Rtsp.RtspClient(CamAURI, Rtsp.RtspClient.ClientProtocolType.Tcp)
+        Dim CamBStream As New Media.Rtsp.RtspClient(CamBURI, Rtsp.RtspClient.ClientProtocolType.Reliable)
+        Dim CamCStream As New Media.Rtsp.RtspClient(CamCURI, Rtsp.RtspClient.ClientProtocolType.Reliable)
+
+        Debug.WriteLine("[Media] Initiating RTSP Connections...")
+
+        Try
+            CamAStream.Connect()
+            Debug.WriteLine("[Media] Feed A is ONLINE!")
+        Catch ex As Exception
+            Debug.WriteLine("[Media] Feed A is OFFLINE! - " & ex.Message)
+        End Try
+
+
+        'Try
+        '    CamBStream.Connect()
+        '    Debug.WriteLine("[Media] Feed B is ONLINE!")
+        'Catch ex As Exception
+        '    Debug.WriteLine("[Media] Feed B is OFFLINE! - " & ex.Message)
+        'End Try
+
+        'Try
+        '    CamCStream.Connect()
+        '    Debug.WriteLine("[Media] Feed C is ONLINE!")
+        'Catch ex As Exception
+        '    Debug.WriteLine("[Media] Feed C is OFFLINE! - " & ex.Message)
+        'End Try
     End Sub
 
     Private Sub HIDPoll_Tick(sender As Object, e As EventArgs) Handles HIDPoll.Tick
